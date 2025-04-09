aa7b99322d16966d0d129fa500776d0e0825538a
aleksey <azhlbn@gmail.com>
2025-04-09 15:07:18 +0300

    Feature: Pause functionality added

--- src/AddressRegistry.sol
+++ src/AddressRegistry.sol
@@ -11,6 +11,8 @@ import {AccessControlUpgradeable} from "@openzeppelin-upgradeable/contracts/acce
 contract AddressRegistry is AccessControlUpgradeable {
     // State variables
     mapping(bytes32 => address) private _addresses;
+    bool private _totemsArePaused;
+    bool private _ecosystemPaused;
 
     // Constants
     bytes32 private constant MANAGER = keccak256("MANAGER");
@@ -18,7 +20,8 @@ contract AddressRegistry is AccessControlUpgradeable {
     bytes32 private constant MYTHO_TOKEN = "MYTHO_TOKEN";
     bytes32 private constant MYTHO_TREASURY = "MYTHO_TREASURY";
     bytes32 private constant TOTEM_FACTORY = "TOTEM_FACTORY";
-    bytes32 private constant TOTEM_TOKEN_DISTRIBUTOR = "TOTEM_TOKEN_DISTRIBUTOR";
+    bytes32 private constant TOTEM_TOKEN_DISTRIBUTOR =
+        "TOTEM_TOKEN_DISTRIBUTOR";
 
     // Events
     event AddressSet(
@@ -26,6 +29,8 @@ contract AddressRegistry is AccessControlUpgradeable {
         address oldAddress,
         address newAddress
     );
+    event TotemsPaused(bool isPaused);
+    event EcosystemPaused(bool isPaused);
 
     // Custom errors
     error InvalidIdentifier(bytes32 id);
@@ -54,11 +59,29 @@ contract AddressRegistry is AccessControlUpgradeable {
         address _newAddress
     ) external onlyRole(MANAGER) {
         if (_newAddress == address(0)) revert ZeroAddress();
-        
+
         address oldAddress = _addresses[_id];
         _addresses[_id] = _newAddress;
 
         emit AddressSet(_id, oldAddress, _newAddress);
+    }
+
+    /**
+     * @notice Pauses or unpauses all Totem implementations
+     * @param _isPaused True to pause, false to unpause
+     */
+    function setTotemsPaused(bool _isPaused) external onlyRole(MANAGER) {
+        _totemsArePaused = _isPaused;
+        emit TotemsPaused(_isPaused);
+    }
+
+    /**
+     * @notice Pauses or unpauses the entire ecosystem
+     * @param _isPaused True to pause, false to unpause
+     */
+    function setEcosystemPaused(bool _isPaused) external onlyRole(MANAGER) {
+        _ecosystemPaused = _isPaused;
+        emit EcosystemPaused(_isPaused);
     }
 
     // VIEW FUNCTIONS
@@ -101,7 +124,7 @@ contract AddressRegistry is AccessControlUpgradeable {
      */
     function getTotemTokenDistributor() external view returns (address) {
         return getAddress(TOTEM_TOKEN_DISTRIBUTOR);
-    }    
+    }
 
     /**
      * @notice Gets an address from the registry by its identifier
@@ -110,5 +133,21 @@ contract AddressRegistry is AccessControlUpgradeable {
      */
     function getAddress(bytes32 _id) public view returns (address) {
         return _addresses[_id];
+    }
+
+    /**
+     * @notice Checks if all Totems are paused
+     * @return True if Totems are paused, false otherwise
+     */
+    function areTotemsPaused() external view returns (bool) {
+        return _totemsArePaused;
+    }
+
+    /**
+     * @notice Checks if the entire ecosystem is paused
+     * @return True if the ecosystem is paused, false otherwise
+     */
+    function isEcosystemPaused() external view returns (bool) {
+        return _ecosystemPaused;
     }
 }
